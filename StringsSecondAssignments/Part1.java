1bf3e54aef6483f248d5cf328842fcadb80bd2cd
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-22 09:58:37 -0300

    Resoluções dos exercicios da segunda semana.

--- StringsSecondAssignments/Part1.java
+++ StringsSecondAssignments/Part1.java
@@ -7,8 +7,68 @@
  */
 public class Part1 {
     
-    public static Integer findStopCodon(String dna, Int startIndex, String stopCodon) {
+    public Integer findStopCodon(String dna, int startIndex, String stopCodon) {
         
+        int stopIndex = dna.indexOf(stopCodon, startIndex + 3);
+        if (stopIndex != -1 && (startIndex - stopIndex) % 3 == 0) {
+            return stopIndex;
+        }
+        else {
+            return dna.length();
+        }
+    }
+    
+    public void testFindStopCodon() {
+        
+        String dna = "AATGATACGTAATAGGGT";
+        System.out.println("DNA strand is " + dna);
+        int gene = findStopCodon(dna, 1, "TTA");
+        if (gene == dna.length()) {
+            System.out.println("No valid genes. DNA chain length is " + gene);
+        }
+        else {
+            System.out.println("The first stop Codon is " + gene + " index.");
+        }
+        
+        dna = "AATGCTAGGGTTATATGGT";
+        System.out.println("DNA strand is " + dna);
+        gene = findStopCodon(dna, 1, "TTA");
+        if (gene == dna.length()) {
+            System.out.println("No valid genes. DNA chain length is " + gene);
+        }
+        else {
+            System.out.println("The first stop Codon is " + gene + " index.");
+        }
+
+        dna = "ATGCTATACTTCGGCTGCTCTTATATTGT";
+        System.out.println("DNA strand is " + dna);
+        gene = findStopCodon(dna, 0, "TTA");
+        if (gene == dna.length()) {
+            System.out.println("No valid genes. DNA chain length is " + gene);
+        }
+        else {
+            System.out.println("The first stop Codon is " + gene + " index.");
+        }
+        
+        dna = "ATCCTATGCTTCGGCTGCTCTAATATGGT";
+        System.out.println("DNA strand is " + dna);
+        gene = findStopCodon(dna, 5, "TTA");
+        if (gene == dna.length()) {
+            System.out.println("No valid genes. DNA chain length is " + gene);
+        }
+        else {
+            System.out.println("The first stop Codon is " + gene + " index.");
+        }
+
+        dna = "ATGATTGATAA";
+        System.out.println("DNA strand is " + dna);
+        gene = findStopCodon(dna, 0, "TTA");
+        if (gene == dna.length()) {
+            System.out.println("No valid genes. DNA chain length is " + gene);
+        }
+        else {
+            System.out.println("The first stop Codon is " + gene + " index.");
+        }
     }
 
 }

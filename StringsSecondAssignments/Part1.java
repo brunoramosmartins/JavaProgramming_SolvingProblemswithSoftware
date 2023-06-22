fb1314ca0dcf4f043011413004f0734cd5f2efd8
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-06-21 22:52:19 -0300

    Inicio da ultima tarefa Week2

---
+++ StringsSecondAssignments/Part1.java
@@ -0,0 +1,14 @@
+
+/**
+ * Escreva a descrição da classe Part1 aqui.
+ * 
+ * @author Bruno Ramos Martins 
+ * @version (número de versão ou data)
+ */
+public class Part1 {
+    
+    public static Integer findStopCodon(String dna, Int startIndex, String stopCodon) {
+        
+    }
+
+}

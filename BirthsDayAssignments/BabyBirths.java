065bcd854c7a59b0f35836a2c2427feab444f664
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-07-01 11:18:21 -0300

    Início da solução da semana.

--- BirthsDayAssignments/BabyBirths.java
+++ BirthsDayAssignments/BabyBirths.java
@@ -1,6 +1,7 @@
 
 /**
- * Escreva a descrição da classe BabyBirths aqui.
+ * This program was developed to analyze the most popular names in a specific 
+ * country throughout the years.
  * 
  * @author Bruno Ramos Martins 
  * @version 01-07-2023
@@ -14,11 +15,36 @@ public class BabyBirths {
     public void printNames() {
         FileResource fr = new FileResource();
         for (CSVRecord rec : fr.getCSVParser(false)) {
-            System.out.println("Name " + rec.get(0) + 
-                               " Gender " + rec.get(1) + 
-                               " Num Born " + rec.get(2));
+            int numBorn = Integer.parseInt(rec.get(2));
+            if (numBorn <= 100) {
+                System.out.println("Name " + rec.get(0) + 
+                                   " Gender " + rec.get(1) + 
+                                   " Num Born " + rec.get(2));
+            }
         }
     }
     
-
+    public void totalBirths (FileResource fr) {
+        int totalBirths = 0;
+        int totalBoys = 0;
+        int totalGirls = 0;
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            int numBorn = Integer.parseInt(rec.get(2));
+            totalBirths += numBorn;
+            if (rec.get(1).equals("M")) {
+                totalBoys += numBorn;
+            }
+            else {
+                totalGirls += numBorn;
+            }
+        }
+        System.out.println("total births = " + totalBirths);
+        System.out.println("total girls = " + totalGirls);
+        System.out.println("total boys = " + totalBoys);
+    }
+    
+    public void testTotalBirths() {
+        FileResource fr = new FileResource();
+        totalBirths(fr);
+    }
 }
